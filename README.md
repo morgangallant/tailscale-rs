b02745d30492d899566e66407f6e1e5dae61b8c3
Morgan Gallant <morgan@morgangallant.com>
2020-06-08 20:54:10 -0400

    tailscale: v0.1.0
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# tailscale-rs
-Rust client library for @tailscale.
+__tailscale-rs__ is an unofficial client library for [Tailscale](https://tailscale.com). It's purpose is to provide application-level primitives for accessing the Tailscale interface of a machine, as well as utilities for building distributed systems such as automatic peer discovery. Feature requests are welcome and encouraged!
+
+All versions v0.X.X are unofficial, and if Tailscale, Inc. chooses to create an official client library, the ownership of this crate will be transferred to them to release and maintain V1+. At this time, breaking API changes may or may be introduced.
+
+### Changelog
+
+All changes to this crate over time will be documented here.
+
+#### V0.1.0
+
+- Added the ability to query for a machine's Tailscale interface IPv4 address.
+- Published to crates.io.
+
---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+//! tailscale-rs is a client library for [Tailscale](https://tailscale.com).
+//!
+//! All versions v0.X.X are unofficial, and if Tailscale, Inc. chooses
+//! to create an official client library, the ownership of this crate
+//! will be transferred to them to release and maintain V1+. At this
+//! time, breaking API changes may or may be introduced.
+extern crate ipnetwork;
+extern crate pnet;
+
+use ipnetwork::IpNetwork;
+use pnet::datalink;
+use std::net::IpAddr;
+
+fn maybe_tailscale(s: &str) -> bool {
+    s.starts_with("wg")
+        || s.starts_with("ts")
+        || s.starts_with("tailscale")
+        || s.starts_with("utun")
+}
+
+/// Retrieve the IP address of the current machine's Tailscale interface, if any.
+/// ```
+/// let iface = tailscale::interface().expect("no tailscale interface found");
+/// ```
+pub fn interface() -> Option<IpAddr> {
+    let ifaces = datalink::interfaces();
+    let netmask: IpNetwork = "100.64.0.0/10".parse().unwrap();
+    ifaces
+        .iter()
+        .filter(|iface| maybe_tailscale(&iface.name))
+        .map(|iface| iface.ips.clone())
+        .flatten()
+        .filter(|ipnet| ipnet.is_ipv4() && netmask.contains(ipnet.network()))
+        .map(|ipnet| ipnet.ip())
+        .next()
+}
